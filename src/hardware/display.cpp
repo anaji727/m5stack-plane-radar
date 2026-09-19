e82946e4e7ac1d14a30d06e2735c4ba64f059fa1
anaji727 <anaji727@gmail.com>
2026-09-20 02:56:44 +0900

    fix: rotate display 90 degrees clockwise

--- src/hardware/display.cpp
+++ src/hardware/display.cpp
@@ -8,7 +8,7 @@ void displayInit() {
   cfg.internal_spk = false;
   cfg.internal_mic = false;
   M5.begin(cfg);
-  tft.setRotation(0); // M5Stack's ILI9342 is natively 320x240.
+  tft.setRotation(1); // Rotate clockwise 90 degrees: portrait 240x320.
   tft.setBrightness(80);
   tft.setTextWrap(false);
   displayFontInit();
