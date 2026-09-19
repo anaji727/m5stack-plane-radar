caacd8fb36376cf6d5c84cda18df84ac66c94528
anaji727 <anaji727@gmail.com>
2026-09-19 20:08:33 +0900

    Add M5Unified display and font support

---
+++ src/hardware/display.cpp
@@ -0,0 +1,15 @@
+#include "hardware/display.h"
+#include "hardware/display_font.h"
+M5GFX& tft = M5.Display;
+void displayInit() {
+  auto cfg = M5.config();
+  cfg.internal_imu = false;
+  cfg.internal_rtc = false;
+  cfg.internal_spk = false;
+  cfg.internal_mic = false;
+  M5.begin(cfg);
+  tft.setRotation(0); // M5Stack's ILI9342 is natively 320x240.
+  tft.setBrightness(80);
+  tft.setTextWrap(false);
+  displayFontInit();
+}
